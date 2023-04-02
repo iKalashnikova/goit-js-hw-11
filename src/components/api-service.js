2fba5efcaba696da3908193b68c3b001d76106c6
iKalashnikova <ikalashnikova@ukr.net>
2023-04-02 14:10:23 +0200

    problem

--- src/components/api-service.js
+++ src/components/api-service.js
@@ -10,7 +10,7 @@ console.log(this);
         const URl = "https://pixabay.com/api/";
         const KEY = "34967949-bc4aa4b6b9ade32e48c05a514";
         
-        fetch(`${URl}?key=${KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}=1&per_page=5`)
+        fetch(`${URl}?key=${KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=5`)
             .then((res) => res.json())
             .then(data => {
                 console.log(data);
